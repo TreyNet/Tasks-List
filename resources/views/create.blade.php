aa2b61079686c598d85c883c0a101eda1cfa5cc0
Javier García Sanjuán <124883921+TreyNet@users.noreply.github.com>
2025-05-13 16:18:33 +0200

    Doc in create.blade.php

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,5 +1,11 @@
-@extends('layouts.app')
+{{-- 
+    Description:
+    This view extends a main layout and includes a form partial.
+    It's commonly used to render a page with a form while maintaining a consistent layout across the application.
+--}}
 
-@section('content')
-    @include('form')
-@endsection
+@extends('layouts.app') {{-- Extend the main layout named "app" located in resources/views/layouts/app.blade.php --}}
+
+@section('content')     {{-- Define the section named "content" which will be injected into the layout --}}
+    @include('form')    {{-- Include the form partial view located at resources/views/form.blade.php --}}
+@endsection             {{-- End of the "content" section --}}
