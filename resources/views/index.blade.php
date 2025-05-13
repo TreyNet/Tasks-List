3ca4fa9203ae92e29b42cfb46e8cae47f16f6328
Javier García Sanjuán <124883921+TreyNet@users.noreply.github.com>
2025-05-13 16:28:19 +0200

    Doc index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,26 +1,45 @@
+{{-- 
+    Blade Template: Tasks List View
+
+    Description:
+    This view displays a list of tasks. It provides a link to create a new task, 
+    loops through the tasks to display each one, and shows pagination links if there are any tasks.
+    Tasks marked as "completed" are displayed with a strikethrough style.
+--}}
+
+{{-- Extends the base layout from resources/views/layouts/app.blade.php --}}
 @extends('layouts.app')
 
+{{-- Sets the page title to "Tasks List" --}}
 @section('title', 'Tasks List')
 
+{{-- Starts the "content" section to be injected into the layout --}}
 @section('content')
-
+    {{-- Link to the task creation page --}}
     <nav class="mb-4">
         <a href={{ route('tasks.create') }} class="font-medium text-gray-700 underline decoration-pink-500">Add Task</a>
     </nav>
-
+    {{-- Loop through each task --}}
     @forelse($tasks as $task)
         <div>
+            {{-- 
+                Link to view a single task's details.
+                The 'line-through' class is conditionally applied if the task is completed.
+            --}}
             <a href="{{ route('task.show', ['task' => $task->id]) }}" @class(['line-through' => $task->completed])>{{ $task->title }}</a>
 
         </div>
     @empty
+        {{-- Message shown if no tasks are found --}}
         <div>There are no tasks.</div>
     @endforelse
 
     @if ($tasks->count())
+        {{-- Pagination navigation, shown only if there are tasks --}}
         <nav class="mt-4">
             {{ $tasks->links() }}
         </nav>
     @endif
 
+{{-- Ends the "content" section --}}
 @endsection
